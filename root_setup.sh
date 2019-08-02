1924c3d505d122c80167c16e462d6535200610e3
Artëm Abashev <artem@abashev.com>
2019-08-02 17:55:52 +0300

    Update root_setup.sh

--- root_setup.sh
+++ root_setup.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 apt-get update
 
+apt-get install apt-transport-https ca-certificates curl software-properties-common
+
 apt-get install -y xorg # X display server https://en.wikipedia.org/wiki/X.Org_Server
 
 # installing and configuring nvidia DRIVERS
 apt-get install build-essential -y
-curl -O https://download.nvidia.com/XFree86/Linux-x86_64/352.79/NVIDIA-Linux-x86_64-352.79.run
-chmod +x ./NVIDIA-Linux-x86_64-*.run
-./NVIDIA-Linux-x86_64-*.run -q -a -n -X -s
+apt-get install nvidia-driver-390 -y
 
 # xorg conf generated via nvidia-xconfig --allow-empty-initial-configuration
 # overwriting xorg.conf adding the BusID where the video card is installed
@@ -72,31 +72,11 @@ EndSection
 EOF
 
 # Docker
-apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
-apt-add-repository 'deb https://apt.dockerproject.org/repo ubuntu-xenial main'
-apt-get update -y
-apt-get install -y docker-engine
-# 'systemctl status docker' to check the service status
-# allow docker to be used without sudo - THIS REQUIRES TO LOGOUT AND LOGIN AGAIN!!!
-usermod -aG docker ubuntu # assuming "ubuntu" is the user name
-
-# Nvidia docker https://github.com/NVIDIA/nvidia-docker/wiki
-export NVIDIADOCKER_VERSION=1.0.1
-wget -P /tmp https://github.com/NVIDIA/nvidia-docker/releases/download/v${NVIDIADOCKER_VERSION}/nvidia-docker_${NVIDIADOCKER_VERSION}-1_amd64.deb
-dpkg -i /tmp/nvidia-docker*.deb && rm /tmp/nvidia-docker*.deb
-# Test nvidia-smi
-# nvidia-docker run --rm nvidia/cuda nvidia-smi
-
-# Docker compose
-curl -L https://github.com/docker/compose/releases/download/1.8.1/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
-rm -f /usr/bin/docker-compose
-ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
-
-# Nvidia docker compose https://github.com/eywalker/nvidia-docker-compose
-apt-get install python-pip -y
-pip install --upgrade pip
-pip install nvidia-docker-compose
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+apt-get update
+apt-get install docker-ce -y
+usermod -aG docker ubuntu
 
 # install virtualgl
 export VGL_VERSION=2.5.2
